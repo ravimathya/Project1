da857fc46bec8d5804d67dc3ce4a18e546fded25
ravimathya <ravimathya@gmail.com>
2019-04-04 17:22:17 +0545

    adding mapping method

---
+++ js/array_methods_mp.js
@@ -0,0 +1,68 @@
+var students = [
+    {
+        name: "Ram",
+        college: "ktm",
+        dob: "123235"
+    },
+    {
+        name: "Rame",
+        college: "ktmd",
+        dob: "123235"
+    },
+    {
+        name: "Ramesh",
+        college: "ktmdo",
+        dob: "123235"
+    }
+];
+
+//array.map()
+//second = current index, third=array under evaluation
+//JSON format
+
+/* students.map(function(student, index , array) {
+    console.log(index + " " + student.name);
+
+});
+
+var mappedData = students.map(function(student){
+    return student.dob;
+})
+
+var arrayOne = [1,2,3,4,5];
+var square = arrayOne.map(function(arr) {
+    return arr**2;
+})
+console.log(square);
+console.log(mappedData); */
+
+
+
+/* var studentList = document.querySelector("#student_list");
+
+students.forEach(function(student ,index) {
+    var listEl = document.createElement("li");
+    listEl.innerText = `${index + 1} ${student.name}`;
+    studentList.appendChild(listEl);
+}); */
+
+
+
+var studentTableBody = document.querySelector("#student-table");
+students.forEach(function(student ,index) {
+    var tableTr = document.createElement("tr");
+    var tableTd1 = document.createElement("td");
+    tableTd1.innerText = index +1;
+    var tableTd2 = document.createElement("td");
+    tableTd2.innerText = student.name;
+    var tableTd3 = document.createElement("td");
+    tableTd3.innerText = student.college;
+    var tableTd4 = document.createElement("td");
+    tableTd4.innerText = student.dob;
+    tableTr.appendChild(tableTd1);
+    tableTr.appendChild(tableTd2);
+    tableTr.appendChild(tableTd3);
+    tableTr.appendChild(tableTd4);
+    studentTableBody.appendChild(tableTr);
+
+})
--- js/main.js
+++ js/main.js
@@ -33,5 +33,93 @@ for(let i = 0; i<testDivEl.length; i++) {
 //3. querySelector(), querySelectorAll()
 
 
-var cardsEl = document.querySelector('.card:last-child');
-console.log(cardsEl);
+/* var cardsEl = document.querySelector('.card:last-child');
+console.log(cardsEl); */
+
+
+// parent child
+
+/* var childEl = document.getElementById("testOne");
+console.dir(childEl);
+
+var parentEl = childEl.parentNode;
+
+console.dir(parentEl.children); */
+
+/* function handleBtnClick(el) {
+
+    el.innerText = "Clicked";
+    console.dir(el);
+;} */
+
+/* var clickBtn = document.getElementById("clickBtn");
+var childEl = document.getElementById("testOne");
+
+clickBtn.onclick = function() {
+    childEl.style.color = 'red';
+    childEl.style.fontWeight = 'bold';
+    childEl.style.fontsize = '50px';
+    console.log("CLickeeeeeeeeeeeeeeed");
+
+    clickBtn.innerText= "Nicely changeed";
+
+} */
+
+//p, div, span, basically any html element
+
+/* let d = document;
+
+var newP = d.createElement("p");
+//assign new class name to newly created paragraph element
+newP.className = "normal";
+//setting inner text of paragraph element
+newP.innerText = "This is Append paragraph";
+console.dir(newP);
+
+//setting up new attribute and its value;
+
+newP.setAttribute("id", "newID");
+
+
+var newEl = d.getElementById("new_element");
+newEl.appendChild(newP);
+
+var textGemeratorBtn = d.getElementById("text-generator");
+var generatedTextDiv = d.getElementById("generated-text");
+
+textGenerator.onclick = function() {
+    var newTextPara = document.createElement('p');
+    newTextPara.innerText = "This is newly generated text.";
+    generatedTextDiv.appendChild(newTextPara);
+}
+
+var dblClick = d.querySelector("doubleClickBtn");
+doubleCliclBtn.ondblclick = function() {
+    console.log(dblClick);
+} */
+
+
+var myInput = document.querySelector("#id_name");
+
+var typedValue = document.querySelector("#typedValue");
+
+myInput.onchange = function(e) {
+    //e= our event fired = object
+    //target is one of the property of event
+    //target is also an object
+    var result = e.target.value;
+    var integerResult = parseInt(result);
+    if(isNaN(integerResult)) {
+        typedValue.innerText = "Please enter the valid number";
+    }
+    else {
+        typedValue.innerText = result;
+
+    }
+    console.log(integerResult);
+/*     console.log(e);
+    console.log(e.target.value);
+    console.log(typeof result) */
+
+    // typedValue.innerText = result;
+}
